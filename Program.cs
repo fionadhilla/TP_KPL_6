a5e2cc495f1370e8abf474ac36ed66fddc825e7d
fionadhilla <fiona08dhilla@gmail.com>
2024-03-25 00:35:14 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int increaseAmount)
+    {
+        playCount += increaseAmount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    public static void Main(string[] args)
+    {
+        string namaPraktikan = "FIONADHILLA";
+        string judulVideo = $"Tutorial Design By Contract - {namaPraktikan}";
+
+        SayaTubeVideo video = new SayaTubeVideo(judulVideo);
+
+        video.IncreasePlayCount(10);
+
+        video.PrintVideoDetails();
+    }
+}
